c6621cbbdc6a9b871021d26937a05917a24944d4
Kidubo <innocentleonard0007@gmail.com>
2022-03-25 02:11:41 +0300

    Creating AuthGuards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,7 @@ import {
   Post,
   Query,
   Session,
-  UseInterceptors,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -17,24 +17,19 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth/auth.service';
 import { CurrentUSer } from './decorators/current-user.decorator';
-import { CurrentUSerInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './entities/users.entities';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUSerInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
 
-  // @Get('/myprofile')
-  // myprofile(@Session() session: any) {
-  //   return this.usersService.findOne(session.userId);
-  // }
-
   @Get('/myprofile')
+  @UseGuards(AuthGuard)
   myprofile(@CurrentUSer() user: User) {
     return user;
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,6 +10,13 @@ import { CurrentUSerInterceptor } from './interceptors/current-user.interceptor'
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // This step create the repository
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUSerInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR, // This is Global scope interceptor
+      useClass: CurrentUSerInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
